e78c0112eabfadb9dfc953cfa6c7dfecffbe772d
Jafar <jafaralkhafajidev@hotmail.com>
2021-09-18 19:16:39 +0200

    Created function playRound

--- scripts/index.js
+++ scripts/index.js
@@ -1,7 +1,27 @@
-// function computerPlay()
-// ouputs random word from array strings
+let options = ["Rock", "Paper", "Scissors"];
+
 function computerPlay() {
-  let strings = ["Rock", "Paper", "Scissors"];
-  let randomNum = Math.floor(Math.random() * strings.length);
-  return strings[randomNum];
+  let randomNum = Math.floor(Math.random() * options.length);
+  return options[randomNum];
+}
+
+let playerSelection = "";
+function playRound(playerSelection) {
+  let computerSelection = computerPlay();
+
+  if (playerSelection === "Rock" && computerSelection === "Paper") {
+    return "You lose! Paper beats rock!";
+  } else if (playerSelection === "Paper" && computerSelection === "Rock") {
+    return "You win! Paper beats rock!";
+  } else if (playerSelection === "Rock" && computerSelection === "Scissors") {
+    return "You win! Rock beats scissors!";
+  } else if (playerSelection === "Scissors" && computerSelection === "Rock") {
+    return "You lose! Rock beats scissors!";
+  } else if (playerSelection === "Paper" && computerSelection === "Paper") {
+    return "You lose! Scissors beats paper!";
+  } else if (playerSelection === "Scissors" && computerSelection === "Paper") {
+    return "You win! Scissors beats paper!";
+  } else {
+    return "You tied! Try again!";
+  }
 }
